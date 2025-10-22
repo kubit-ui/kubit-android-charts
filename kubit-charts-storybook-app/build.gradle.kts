86ac72ab607d242a327e72364a2a135e5572834f
rulogarcillan <raulrcs@gmail.com>
2025-10-22 13:27:12 +0200

    Add AppCompat dependency and fix range spacing in AxisData
    Added androidx.appcompat dependency to the storybook app and updated version catalog. Fixed spacing in range operator within AxisData.kt for consistency.

--- kubit-charts-storybook-app/build.gradle.kts
+++ kubit-charts-storybook-app/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
 
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.appcompat)
 
     implementation(projects.kubitCharts.samples)
 }
